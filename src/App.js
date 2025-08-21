cec487db330c2a0b8e58b2794cffbdf26e1289a0
unknown <samuel90ezekiel@gmail.com>
2025-08-21 01:05:48 +0100

    moved components to HomePage rm BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-route-dom";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Courses from "./components/Courses";
-import Testimonials from "./components/Testimonials";
-import CTA from "./components/CTA";
-import Footer from "./components/Footer";
+import { Routes, Route  } from 'react-router-dom';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import HomePage from "./pages/HomePage";
+
 
 function App() {
   return (
     <div className="bg-white text-gray-900">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </BrowserRouter>
-      <Navbar />
-      <Hero />
-      <Features />
-      <Courses />
-      <Testimonials />
-      <CTA />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
