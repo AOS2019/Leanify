fe577fbd97eadd2a28e9256a6549eaed11e1e67b
unknown <samuel90ezekiel@gmail.com>
2025-08-21 01:10:42 +0100

    added route Link

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function CTA() {
   return (
@@ -14,20 +15,12 @@ export default function CTA() {
             <div className="flex flex-col items-start justify-between gap-3 sm:flex-row">
               <div className="mb-10 text-sm">No credit card required • Cancel anytime</div>
               <div className="flex w-full gap-2 sm:w-auto">
-                <button className="bg-white text-indigo-600 px-6 py-3 rounded-lg w-full sm:w-auto">Create free account</button>
-                <button className="border border-white px-6 py-3 rounded-lg w-full sm:w-auto">See pricing</button>
+                <Link to="/register"><button className="bg-white text-indigo-600 px-6 py-3 rounded-lg w-full sm:w-auto">Create free account</button></Link>
+                <Link to="/"><button className="border border-white px-6 py-3 rounded-lg w-full sm:w-auto">See pricing</button></Link>
               </div>
             </div>
           </div>
         </div>
     </section>
-    // <section className="py-16 bg-indigo-600 text-white text-center">
-    //   <h2 className="text-3xl font-bold mb-4">Start Learning Today</h2>
-    //   <p className="mb-6">Your first week is free. No credit card required.</p>
-    //   <div className="flex flex-col sm:flex-row gap-4 justify-center">
-    //     <button className="bg-white text-indigo-600 px-6 py-3 rounded-lg">Create Free Account</button>
-    //     <button className="border border-white px-6 py-3 rounded-lg">See Pricing</button>
-    //   </div>
-    // </section>
   );
 }
