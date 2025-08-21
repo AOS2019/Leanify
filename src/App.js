7fed89a0b1608d26b0b64275363bbd645845bdc1
unknown <samuel90ezekiel@gmail.com>
2025-08-21 04:33:51 +0100

    Added Dashboard and protected Route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { Routes, Route  } from 'react-router-dom';
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
 
 
 function App() {
   return (
     <div className="bg-white text-gray-900">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+          {/* Public */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Protected */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            } />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
