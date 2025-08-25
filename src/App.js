b3813234ce8a53b0f88a38978722a2b3f0d57115
unknown <samuel90ezekiel@gmail.com>
2025-08-25 02:41:24 +0100

    price and course path

--- src/App.js
+++ src/App.js
@@ -1,46 +1,54 @@
 import React from "react";
 import { Routes, Route  } from 'react-router-dom';
 import { AuthProvider } from "./context/AuthContext";
+import { UserProvider } from "./context/UserContext";
 // import ProtectedRoute from "./components/ProtectedRoute";
 import PrivateRoute from "./components/PrivateRoute";
-
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import HomePage from "./pages/HomePage";
 import LearnerDashboard from "./pages/LearnerDashboard";
 import TutorDashboard from "./pages/TutorDashboard";
+import BrowseCourses from "./pages/BrowseCourses";
+import Pricing from "./pages/Pricing";
 
 
 function App() {
   return (
     <div className="bg-white text-gray-900">
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
+      <UserProvider>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
 
-          {/* Public */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+            {/* Public */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
 
-          {/* Protected Learner Dashboard */}
-          <Route
-            path="/learner-dashboard"
+             {/* Public marketing pages */}
+            <Route path="/courses" element={<BrowseCourses />} />
+            <Route path="/pricing" element={<Pricing />} />
+
+            {/* Protected Learner Dashboard */}
+            <Route
+              path="/learner-dashboard"
+              element={
+                <PrivateRoute role="learner">
+                  <LearnerDashboard />
+                </PrivateRoute>
+              } />
+
+            {/* Protected Tutor Dashboard */}
+            <Route
+            path="tutor-dashboard"
             element={
-              <PrivateRoute role="learner">
-                <LearnerDashboard />
+              <PrivateRoute role="tutor">
+                <TutorDashboard />
               </PrivateRoute>
             } />
-
-          {/* Protected Tutor Dashboard */}
-          <Route
-          path="tutor-dashboard"
-          element={
-            <PrivateRoute role="tutor">
-              <TutorDashboard />
-            </PrivateRoute>
-          } />
-        </Routes>
-      </AuthProvider>
+          </Routes>
+        </AuthProvider>
+      </UserProvider>  
     </div>
   );
 }
